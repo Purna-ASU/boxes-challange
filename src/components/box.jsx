9de5c0f53550ed3ab24a206dd4799e732f0dcfa4
Purna-ASU <ppeddire@asu.edu>
2023-08-16 14:35:22 -0700

    Challenge 3.1: Declaring the state within the conponent

--- src/components/box.jsx
+++ src/components/box.jsx
@@ -1,13 +1,20 @@
+import React from 'react';
 
 function box(props) {
 
+    const [on,setOn] = React.useState(props.on)
+
     const styles = {
-        backgroundColor: props.on ? "#222222" : "none" 
+        backgroundColor: on ? "#222222" : "transparent" 
+    }
+
+    function toggle() {
+        setOn(prevOn => !prevOn)
     }
 
     return (
         <>
-            <div style={styles} className='box'></div>
+            <div style={styles} className='box' onClick={toggle}></div>
         </>
     )
 }
